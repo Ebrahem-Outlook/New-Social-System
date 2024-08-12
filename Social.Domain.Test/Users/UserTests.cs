d93bd494cdb3d91aec9365af7295432304fdd337
EbrahemElbetar1@outlook.com <EbrahemElbetar1@outlook.com>
2024-08-12 20:03:23 +0300

    V_8

---
+++ Social.Domain.Test/Users/UserTests.cs
@@ -0,0 +1,44 @@
+﻿using Social.Domain.Users;
+using Social.Domain.Users.Events;
+
+namespace Social.Domain.Test.Users;
+
+public class UserTests
+{
+    [Fact]
+    public void CreateUser_ShouldInitializePropertiesCorrectly_AndRaiseUserCreatedEvent()
+    {
+        // Arrange
+        var firstName = "Ebrahem";
+        var lastName = "Mohamed";
+        var userName = "Ebrahem_Outlook";
+        var email = "Ebrahem@gmail.com";
+        var passwordHash = "HashedPassword";
+
+        // Act
+        var user = User.Create(firstName, lastName, userName, email, passwordHash);
+
+        // Assert
+        Assert.Equal(firstName, user.FirstName);
+        Assert.Equal(lastName, user.LastName);
+        Assert.Equal(userName, user.UserName);
+        Assert.Equal(email, user.Email);
+        Assert.Equal(passwordHash, user.PasswordHash);
+    }
+
+    [Fact]
+    public void UpdateUser_ShouldUpdateFirstNameAndLastName_AndRaiseUserUpdatedEvent()
+    {
+        // Arrange
+        var user = User.Create("Ebrahem", "Mohamed", "Ebrahem_Outlook", "Ebrahem@gmail.com", "HashedPassword");
+        var newFirstName = "Omar";
+        var newLastName = "Ahemad";
+
+        // Act
+        user.UpdateUser(newFirstName, newLastName);
+
+        // Assert
+        Assert.Equal(newFirstName, user.FirstName);
+        Assert.Equal(newLastName, user.LastName);
+    }
+}
